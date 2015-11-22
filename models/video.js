19cb5ba7367305e9145a32fc33ae4e61e4841203
Carryl Sorene <carrylsorene@gmail.com>
2015-11-22 15:25:59 +0000

    Video and User forms

--- models/video.js
+++ models/video.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose')
 
 var videoSchema = new mongoose.Schema({
-  title: {type: String, required: true},
+  videoTitle: {type: String, required: true},
+  videoDescription: {type: String, required: true},
   videoId: {type: String, required: true},
   user: {type: String, required: true},
   challenge: {type: mongoose.Schema.ObjectId, ref: 'Challenge'}
